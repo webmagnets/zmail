2559948f0dcc135c7a1654030f7a8e1e5ece4337
Yung Ju Rick Kim <yungjurick@gmail.com>
2021-07-14 01:31:45 +0900

    Initialize project with next.js and redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
 
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import configureStore from '../config/store';
+const {store, persistor} = configureStore();
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  )
 }
 
 export default MyApp
