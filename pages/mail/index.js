f1871ebfe307dbf84c08e348fa1b54aa33fd1f42
Yung Ju Rick Kim <yungjurick@gmail.com>
2021-07-14 14:06:11 +0900

    Fix releative path bug

--- pages/mail/index.js
+++ pages/mail/index.js
@@ -1,10 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import MailLayout from '../components/MailLayout';
-import MailActionContainer from '../components/Mail/MailActionContainer';
-import MailContainer from '../components/Mail/MailContainer';
+import MailLayout from '../../components/MailLayout';
+import MailActionContainer from '../../components/Mail/MailActionContainer';
+import MailContainer from '../../components/Mail/MailContainer';
 
 export default function Mail() {
+  /*
+    use hash routing for directing different categories
+    such as inbox, starred, important, etc.
+  */
+
   return (
     <div>
       <Head>
